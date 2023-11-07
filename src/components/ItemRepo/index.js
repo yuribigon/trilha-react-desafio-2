535e8fcda77468569ef738d579320daa07cd984e
yuribigon <yuri.bigon@gmail.com>
2023-11-07 15:44:57 -0300

    first commit

--- src/components/ItemRepo/index.js
+++ src/components/ItemRepo/index.js
@@ -13,7 +13,7 @@ function ItemRepo({repo, handleRemoveRepo}) {
         <h3>{repo.name}</h3>
         <p>{repo.full_name}</p>
         <a href={repo.html_url} rel="noreferrer" target="_blank">Ver repositório</a><br />
-        <a href="#"  rel="noreferrer" className="remover">Remover</a>
+        <a href={handleRemove}  className="remover">Remover</a>
         <hr />
     </ItemContainer>
   )
--- src/pages/App.js
+++ src/pages/App.js
@@ -36,7 +36,10 @@ function App() {
   const handleRemoveRepo = (id) => {
     console.log('Removendo registro', id);
 
-    // utilizar filter.
+    const updatedRepos = repos.filter(repo => repo.id !== id);
+    setRepos(updatedRepos);
+
+    alert('O repositório foi removido com sucesso!')
   }
 
 
